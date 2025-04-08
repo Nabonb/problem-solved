d6c1fd80e9389eebea875c65203792b24346d92b
Nabonb <nabobaruanb@gmail.com>
2025-04-08 18:54:33 +0600

    element to replace solved

--- elementToReplace.js
+++ elementToReplace.js
@@ -1,1 +1,13 @@
-//
+//Give an array of integers, replace all the occurrences of elementToReplace with subtractionElem.
+
+function elementToReplace(inputArray, elementToReplace, subtractionElem) {
+  inputArray.forEach((element, index) => {
+    if (element === elementToReplace) {
+      console.log(element);
+      inputArray[index] = subtractionElem;
+    }
+  });
+  console.log(inputArray);
+}
+
+elementToReplace([1, 2, 1], 1, 3);
