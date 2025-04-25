c5b5ce4fd0a2c5dcc47c0e7268e689411d610073
Nabonb <nabobaruanb@gmail.com>
2025-04-25 14:25:29 +0600

    ispalindrome problem solved

---
+++ isPalindrome.js
@@ -0,0 +1,16 @@
+// 10. Palindrome Number
+function isPalindrome(num) {
+  let numtoString = num.toString();
+  let ans;
+
+  for (let i = 0; i < numtoString.length; i++) {
+    if (numtoString[i] === numtoString[numtoString.length - 1 - i]) {
+      ans = true;
+    } else {
+      return false;
+    }
+  }
+  return ans;
+}
+
+console.log(isPalindrome(344));
