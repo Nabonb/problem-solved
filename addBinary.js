10ac986ad3b4c2148299b3df81296cd41b866d64
Nabonb <nabobaruanb@gmail.com>
2025-05-13 15:46:38 +0600

    some problem solved

---
+++ addBinary.js
@@ -0,0 +1,7 @@
+//60. Add Binary
+
+function addBinary(a, b) {
+  return (BigInt("0b" + a) + BigInt("0b" + b)).toString(2);
+}
+
+console.log(addBinary("11", "1"));
---
+++ mySqrt.js
@@ -0,0 +1,8 @@
+//61. Sqrt(x)
+
+function mySqrt(x) {
+  let sqrt = Math.sqrt(x);
+  return Math.floor(sqrt);
+}
+
+console.log(mySqrt(4));
