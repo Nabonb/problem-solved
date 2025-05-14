15b5a7205525acc813d835669bc96b0a2e39c689
Nabonb <nabobaruanb@gmail.com>
2025-05-14 13:40:51 +0600

    One solution is changed

--- mySqrt.js
+++ mySqrt.js
@@ -1,8 +1,18 @@
 //61. Sqrt(x)
 
 function mySqrt(x) {
-  let sqrt = Math.sqrt(x);
-  return Math.floor(sqrt);
+  // let sqrt = Math.sqrt(x);
+  // return Math.floor(sqrt);
+  let ans = 0;
+
+  for (let i = 1; i <= x; i++) {
+    if (i * i <= x) {
+      ans = i;
+    } else {
+      break;
+    }
+  }
+  return ans;
 }
 
-console.log(mySqrt(4));
+console.log(mySqrt(0));
